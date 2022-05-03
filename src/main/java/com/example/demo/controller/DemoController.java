67c3321715c96c45df9183097c0caee351e004a5
chanmi910 <chanmi910@gmail.com>
2022-05-03 10:29:12 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+
+    private Environment env;
+
+    @Autowired
+    public DemoController (Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/welcome")
+    public String status() {
+        return String.format("It's Working in Demo Service"
+                + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                + ", port(server.port)=" + env.getProperty("server.port"));
+    }
+}
---
+++ src/test/java/com/example/cicdtekton/CicdTektonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdtekton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTektonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
